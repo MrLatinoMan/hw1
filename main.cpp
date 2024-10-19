b565dd751ba43f1410027e7955372a8b669c8f22
MrLatinoMan <147659671+MrLatinoMan@users.noreply.github.com>
2024-10-19 02:46:48 +0000

    Initial commit for hw1

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int a;
+    cout << "Enter value for a: ";
+    cin >> a;
+    cout << "a = " << a << endl;
+    cout << "2*a = " << 2*a << endl;
+    return 0;
+}
